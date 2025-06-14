b5069f41688fd07e3e66c0c63c7c8ffe68e0ee8f
 <preciada.t@gmail.com>
2025-06-15 04:10:59 +0900

    showアクションでのリダイレクトURLにアカウント確認の成功/失敗フラグを追加

--- app/controllers/custom/confirmations_controller.rb
+++ app/controllers/custom/confirmations_controller.rb
@@ -2,7 +2,9 @@ class Custom::ConfirmationsController < DeviseTokenAuth::ConfirmationsController
   def show
     super do |resource|
       if resource.errors.empty?
-        redirect_to(params[:redirect_url], allow_other_host: true) and return
+        redirect_to("#{params[:redirect_url]}?account_confirmation_success=true", allow_other_host: true) and return
+      else
+        redirect_to("#{params[:redirect_url]}?account_confirmation_success=false", allow_other_host: true) and return
       end
     end
   end
