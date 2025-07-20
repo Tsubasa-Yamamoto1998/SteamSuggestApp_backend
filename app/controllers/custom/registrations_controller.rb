4aa45b972ff4a11f2b84a3d51769bf2ddc09e3e2
 <preciada.t@gmail.com>
2025-07-20 21:59:17 +0900

    createメソッドを追加し、リソースの保存と認証トークンの生成を実装

--- app/controllers/custom/registrations_controller.rb
+++ app/controllers/custom/registrations_controller.rb
@@ -1,4 +1,25 @@
 class Custom::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    unless resource.save
+      Rails.logger.debug resource.errors.full_messages
+      render json: { status: "error", errors: resource.errors.full_messages }, status: 422
+      return
+    end
+
+    yield resource if block_given?
+
+    if resource.active_for_authentication?
+      token = resource.create_token
+      resource.save!
+      render_create_success
+    else
+      expire_data_after_sign_in!
+      render_create_success
+    end
+  end
+
   private
 
   def sign_up_params
